8193e3f2f3c076fff793181d9c793fc4b3e70058
javiier507 <profiledev06@gmail.com>
2023-07-26 01:04:53 -0500

    data fetching

--- app/(adminpanel)/kiosks/page.tsx
+++ app/(adminpanel)/kiosks/page.tsx
@@ -1,3 +1,38 @@
-export default function KiosksPage() {
-    return <h1>Kiosks Page!</h1>;
+'use client';
+
+import { Box, Stack } from '@chakra-ui/react';
+
+import { makeRequest } from '../../../src/utils/request';
+
+type Post = {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+};
+
+async function getData() {
+    return makeRequest<Post[]>(
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            cache: 'no-store',
+        }),
+    );
+}
+
+export default async function KiosksPage() {
+    const { data, error } = await getData();
+    return (
+        <Stack px={8}>
+            <h1>Kiosks Page!</h1>
+            {error ? (
+                <strong>Lo sentimos ha ocurrido un error</strong>
+            ) : (
+                <ol>
+                    {data?.map((item) => (
+                        <li key={item.id}>{item.title}</li>
+                    ))}
+                </ol>
+            )}
+        </Stack>
+    );
 }
---
+++ src/utils/request.ts
@@ -0,0 +1,28 @@
+type HttpResponse<T> = {
+    data: T | null;
+    error?: any;
+};
+
+export async function makeRequest<T>(promiseRequest: Promise<Response>): Promise<HttpResponse<T>> {
+    return new Promise((resolve) => {
+        promiseRequest
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`Server Error: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((data) =>
+                resolve({
+                    data: data as T,
+                    error: null,
+                }),
+            )
+            .catch((error) =>
+                resolve({
+                    data: null,
+                    error,
+                }),
+            );
+    });
+}
