d69dde29ac9fb82af2415913b8f8e1cbf7428c9d
javiier507 <profiledev06@gmail.com>
2023-07-27 00:48:46 -0500

    saving jwt api

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,6 +1,9 @@
 import type { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
+import { executeRequest } from '@/src/utils/request';
+import { LoginResponse } from '@/src/dtos/Login';
+
 export const authOptions: NextAuthOptions = {
     session: {
         strategy: 'jwt',
@@ -17,10 +20,53 @@ export const authOptions: NextAuthOptions = {
                 password: { label: 'Password', type: 'password' },
             },
             async authorize(credentials) {
-                console.log(credentials);
-                const user = { id: '1', name: 'Admin', email: 'admin@admin.com' };
-                return user;
+                try {
+                    const response = await executeRequest<LoginResponse>(
+                        fetch('http://127.0.0.1:8080/auth/login', {
+                            method: 'POST',
+                            body: JSON.stringify({
+                                email: credentials?.email,
+                                password: credentials?.password,
+                            }),
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                        }),
+                    );
+
+                    return {
+                        id: response.data.user._id.toString(),
+                        name: response.data.user.name,
+                        email: response.data.user.name,
+                        apiJWT: response.data.token,
+                    };
+                } catch (e) {
+                    return null;
+                }
             },
         }),
     ],
+    callbacks: {
+        session: ({ session, token }) => {
+            return {
+                ...session,
+                user: {
+                    ...session.user,
+                    id: token.id,
+                    apiJWT: token.apiJWT,
+                },
+            };
+        },
+        jwt: ({ token, user }) => {
+            if (user) {
+                const u = user as unknown as any;
+                return {
+                    ...token,
+                    id: u.id,
+                    apiJWT: u.apiJWT,
+                };
+            }
+            return token;
+        },
+    },
 };
