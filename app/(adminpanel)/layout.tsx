6edac089285ca3f013d41f7e31ffb2e700617cf1
javiier507 <profiledev06@gmail.com>
2023-07-27 01:40:22 -0500

    adding navbar

--- app/(adminpanel)/layout.tsx
+++ app/(adminpanel)/layout.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import NextLink from 'next/link';
-import { useSession } from 'next-auth/react';
-
-import React, { ReactNode } from 'react';
 import {
     IconButton,
+    Avatar,
     Box,
     CloseButton,
     Flex,
+    HStack,
+    VStack,
     Icon,
     useColorModeValue,
     Text,
@@ -17,9 +17,14 @@ import {
     useDisclosure,
     BoxProps,
     FlexProps,
+    Menu,
+    MenuButton,
+    MenuDivider,
+    MenuItem,
+    MenuList,
     Link,
 } from '@chakra-ui/react';
-import { FiHome, FiMenu, FiList } from 'react-icons/fi';
+import { FiHome, FiMenu, FiChevronDown, FiList } from 'react-icons/fi';
 import { IconType } from 'react-icons';
 
 interface LinkItemProps {
@@ -27,51 +32,30 @@ interface LinkItemProps {
     href: string;
     icon: IconType;
 }
-const LinkItems: Array<LinkItemProps> = [
-    { name: 'Dashboard', href: '/dashboard', icon: FiHome },
-    { name: 'Transactions', href: '/transactions', icon: FiList },
-    { name: 'Kiosks', href: '/kiosks', icon: FiList },
-];
 
-export default function AdminPanelLayout({
-    children, // will be a page or nested layout
-}: {
+interface NavItemProps extends FlexProps {
+    icon: IconType;
     children: React.ReactNode;
-}) {
-    const { data: session } = useSession();
-    console.log(session);
+}
 
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    return (
-        <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
-            <SidebarContent onClose={() => onClose} display={{ base: 'none', md: 'block' }} />
-            <Drawer
-                isOpen={isOpen}
-                placement="left"
-                onClose={onClose}
-                returnFocusOnClose={false}
-                onOverlayClick={onClose}
-                size="full"
-            >
-                <DrawerContent>
-                    <SidebarContent onClose={onClose} />
-                </DrawerContent>
-            </Drawer>
-            <MobileNav display={{ base: 'flex', md: 'none' }} onOpen={onOpen} />
-            <Box ml={{ base: 0, md: 60 }} p="4">
-                {children}
-            </Box>
-        </Box>
-    );
+interface MobileProps extends FlexProps {
+    onOpen: () => void;
 }
 
 interface SidebarProps extends BoxProps {
     onClose: () => void;
 }
 
+const LinkItems: Array<LinkItemProps> = [
+    { name: 'Dashboard', href: '/dashboard', icon: FiHome },
+    { name: 'Transactions', href: '/transactions', icon: FiList },
+    { name: 'Kiosks', href: '/kiosks', icon: FiList },
+];
+
 const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
     return (
         <Box
+            transition="3s ease"
             bg={'blue.700'}
             borderRight="1px"
             borderRightColor={useColorModeValue('gray.200', 'gray.700')}
@@ -120,32 +104,103 @@ const NavItem = ({ name, href, icon }: LinkItemProps) => {
     );
 };
 
-interface MobileProps extends FlexProps {
-    onOpen: () => void;
-}
 const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
     return (
         <Flex
             ml={{ base: 0, md: 60 }}
-            px={{ base: 4, md: 24 }}
+            px={{ base: 4, md: 4 }}
             height="20"
             alignItems="center"
-            bg={'blue.700'}
+            bg={useColorModeValue('white', 'gray.900')}
             borderBottomWidth="1px"
             borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
-            justifyContent="flex-start"
+            justifyContent={{ base: 'space-between', md: 'flex-end' }}
             {...rest}
         >
             <IconButton
-                variant="outline"
+                display={{ base: 'flex', md: 'none' }}
                 onClick={onOpen}
+                variant="outline"
                 aria-label="open menu"
                 icon={<FiMenu />}
             />
 
-            <Text fontSize="2xl" ml="8" fontFamily="monospace" fontWeight="bold" color={'white'}>
+            <Text
+                display={{ base: 'flex', md: 'none' }}
+                fontSize="2xl"
+                fontFamily="monospace"
+                fontWeight="bold"
+                color={'white'}
+            >
                 Kaas
             </Text>
+
+            <HStack spacing={{ base: '0', md: '6' }}>
+                <Flex alignItems={'center'}>
+                    <Menu id="navbar" isLazy>
+                        <MenuButton py={2} transition="all 0.3s" _focus={{ boxShadow: 'none' }}>
+                            <HStack>
+                                <Avatar
+                                    size={'sm'}
+                                    src={
+                                        'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                                    }
+                                />
+                                <VStack
+                                    display={{ base: 'none', md: 'flex' }}
+                                    alignItems="flex-start"
+                                    spacing="1px"
+                                    ml="2"
+                                >
+                                    <Text fontSize="sm">Justina Clark</Text>
+                                </VStack>
+                                <Box display={{ base: 'none', md: 'flex' }}>
+                                    <FiChevronDown />
+                                </Box>
+                            </HStack>
+                        </MenuButton>
+                        <MenuList
+                            bg={useColorModeValue('white', 'gray.900')}
+                            borderColor={useColorModeValue('gray.200', 'gray.700')}
+                        >
+                            <MenuItem>Profile</MenuItem>
+                            <MenuDivider />
+                            <MenuItem>Sign out</MenuItem>
+                        </MenuList>
+                    </Menu>
+                </Flex>
+            </HStack>
         </Flex>
     );
 };
+
+export default function AdminPanelLayout({
+    children, // will be a page or nested layout
+}: {
+    children: React.ReactNode;
+}) {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    return (
+        <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
+            <SidebarContent onClose={() => onClose} display={{ base: 'none', md: 'block' }} />
+            <Drawer
+                isOpen={isOpen}
+                placement="left"
+                onClose={onClose}
+                returnFocusOnClose={false}
+                onOverlayClick={onClose}
+                size="full"
+            >
+                <DrawerContent>
+                    <SidebarContent onClose={onClose} />
+                </DrawerContent>
+            </Drawer>
+            {/* mobilenav */}
+            <MobileNav onOpen={onOpen} />
+            <Box ml={{ base: 0, md: 60 }} p="4">
+                {children}
+            </Box>
+        </Box>
+    );
+}
